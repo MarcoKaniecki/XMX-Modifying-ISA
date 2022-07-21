6c412af90567cd62c72fbe0ef2aa577293aad735
Marco <85970557+TacticalStack@users.noreply.github.com>
2022-07-21 09:09:35 -0300

    updated test file

--- test_files/A1test.asm
+++ test_files/A1test.asm
@@ -1,85 +1,28 @@
-;
-; A1 test program
-; Count number of set and clear bits in Data
-; Exercise 14 XMX instructions
-;
-; ECED 3403
-; 22 06 07
-;
-	org	#FFC0
-STKTOP
+; A2 - test file
 ;
 		org	#80
-Data		word	#FEDC	; Pattern to examine
-SetCount	word	#AA00	; Total bits set in Data (#AAxx; AA0C)
-ClrCount	word	#BB00	; Total bits clear in Data (#BBxx; BB04)
+Data		word	#1234
+moreData	word	#AA01
+someData	word	#2222
+number      word    #0003
 ;
 ; Code
 ;
 	org	#1000
 Start
 ;
-	movls	STKTOP,SP	; SP <- &FFC0
+    movlz   Data,A0     ; A0 <- &Data
+    ld  A0,R0       ; R5 <- [Data]
+    movlz   moreData,A1
+    ld  A1,R1
+    movlz   someData,A2
+    ld  A2,R2
+    movlz   number,A3
+    ld  A3,R3
 ;
-	movlz	Data,A0		; A0 <- &Data
-	ld	A0,R5		; R5 <- [Data]
+    addx.b  R3,R3
+    ADDX    R3,A0
+    addx    A0,R2
+    ADDX    A3,A3
 ;
-	movls	#0,R6		; R6 <- #FF00 (Set count)
-	movls	#0,R7		; R7 <- #FF00 (Clear count)
-;
-Loop
-	cmp	#0,R5		; Finished when R5 is zero
-	cex	eq,$1,$0	;
-	br	DoneLoop
-	bl	Subr
-	br	Loop
-;
-DoneLoop    ADDX    A6,A7 ; THIS IS A COMMENT
-;
-; Handle case when MSbit is zero
-;
-	movl	#10,R7		; R7 <- 16 (upper byte should be untouched FFxx)
-	sub.b	R6,R7		; R7 <- R7 - R6
-;
-	ADDX    A6,A7       ; THIS IS A COMMENT
-	ADDX.b  A6,A7
-	SUBX.w  A6,A7
-	CMPX.b  A6,A7
-;
-    ADDX    $2,R0
-    ADDX    $-1,A0
-;
-    ADDX    #20,A0
-    ADDX    $32,A0
-;
-    ADDX    #8,A0
-    ADDX    $8,A0
-;
-    ADDX    #FF,A0
-    ADDX    $-1,A0
-;
-;
-;
-	str.b	R6,A0,$2	; Setcount <- R6
-	str.b	R7,A0,$4	; Clrcount <- R7
-;
-BusyWait
-	br	BusyWait
-;
-; Checks LSbit for zero/one
-; Shifts R5 right (MSbit <- 0)
-;
-Subr	bit	#1,R5		; Bit 0 state?
-	cex	ne,$1,$1
-; Bit 0 set
-	add.b	#1,R6
-; Bit 0 clear
-	add.b	#1,R7		; Ignored
-;
-; Assume that C is clear from either add
-;
-	rrc	R5		; R5 >> 1
-;
-	swap	LR,PC
-;
-	end	Start
+end	Start
